d2ce6aec774976a74a9e08cd4028c278cf215ae1
Feaxmutch <feaxmutch@gmail.com>
2024-10-19 14:26:19 +0300

    Copyed the old solution

--- DynamicArray_v2/Program.cs
+++ DynamicArray_v2/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string CommandSum = "sum";
+            const string CommandExit = "exit";
+
+            bool isWorking = true;
+            List<int> numbers = new();
+
+            while (isWorking)
+            {
+                Console.Clear();
+
+                foreach (var number in numbers)
+                {
+                    Console.Write($"{number} ");
+                }
+
+                Console.WriteLine($"\nВведите число или {CommandSum}, для суммирования введённых ранее чисел.");
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int newNumber))
+                {
+                    numbers.Add(newNumber);
+                }
+                else if (userInput == CommandSum)
+                {
+                    SumNumbers(numbers);
+                }
+                else if (userInput == CommandExit)
+                {
+                    isWorking = false;
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+        static void SumNumbers(List<int> numbers)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine($"Сумма введенных ранее чисел: {sum}");
         }
     }
 }
